9d53cf1bd79c614d74ca5c8d01bd84d1c4047d28
jhjh81704 <jhjh81704@gmail.com>
2019-04-22 05:15:00 +0800

    Update

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ def handle_message(event):
 
     if msg == 'hi':
         r = 'hi'
-    elif msg = '你吃飯了嗎':
+    elif msg == '你吃飯了嗎':
         r = '還沒'
-        
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text= r))
